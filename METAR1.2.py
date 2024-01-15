47c47e71ce457468f031bd827cfa0c194329ae71
Cesar <53136084+jcesar1990@users.noreply.github.com>
2024-01-15 17:42:47 -0600

    final

--- METAR1.2.py
+++ METAR1.2.py
@@ -1,81 +1,68 @@
-import requests
-import urllib.request
-from bs4 import BeautifulSoup
-from datetime import datetime
-from datetime import date, datetime, timedelta,timezone
 import os
+import time
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 
-#Creamos las carpetas donde se alojaran los datos
+# Function to create directories
 def makedir(path):
     if not os.path.exists(path):
         os.makedirs(path)
-        print("El directorio "+path+" ha sido creado")
+        print("El directorio " + path + " ha sido creado")
     else:
-        print("El directorio "+path+" ya existe")
+        print("El directorio " + path + " ya existe")
 
-pathestaciones="../estaciones"  
-pathfiles="../files"
+# Directories
+pathestaciones = "../estaciones"
+pathfiles = "../files"
 
 makedir(pathestaciones)
 makedir(pathfiles)
 
-url="https://aviationweather.gov/data/metar/?id=MMMX&hours=48&include_taf=yes"
-filehtml= '../estaciones/MMMX.txt'
-filein= '../estaciones/MMMX.txt'
-fileout= '../files/MMMX.txt'
+# URLs and file paths
+url = "https://aviationweather.gov/data/metar/?id=MMMX&hours=48&include_taf=yes"
+fileout = "../files/MMMX.txt"
 
-# #Peticion para compiar los datos del html del METAR usando request y beautifulsoupS
-# try:
-#     # Se realiza la solicitud
-#     response = requests.get(url)
-#     # Verificando la respuesta a la solicitud
-#     if response.status_code == 200:
-#         # Obteniendo contenido del html
-#         html_content = response.text
-#         soup= BeautifulSoup(html_content, "html.parser")
-#         data_container = soup.find('div',{'id':'data-container'})
-#         #Si existe el div
-#         if data_container:
-#              print(data_container.text)
-#         print(data_container)
-#         print("El proceso se completo con exito")
-#     else:
-#          print("Algo fallo")
-# except KeyError as e:
-#         print(f"Se presento un error. Detalles del error: {e}")
-
-# Configuración de Selenium
+# Selenium configuration
 chrome_options = Options()
-#chrome_options.add_argument('--headless')
-chrome_options.add_argument("--disable-gpu")  # Necesario en algunos sistemas
-chrome_options.add_argument("--window-size=1920,1080")  # Tamaño de ventana
-driver_path =  'C:/Users/meteorologia/Documents/sgirpcpythonlinux/chromedriver-win64/chromedriver-win64/chromedriver.exe'
+# chrome_options.add_argument('--headless')
+chrome_options.add_argument("--disable-gpu")  # Necessary on some systems
+chrome_options.add_argument("--window-size=1920,1080")  # Window size
+driver_path = 'C:/Users/meteorologia/Documents/sgirpcpythonlinux/chromedriver-win64/chromedriver-win64/chromedriver.exe'
 service = ChromeService(executable_path=driver_path)
 
-# Crear una instancia de Selenium WebDriver
+# Create a Selenium WebDriver instance
 driver = webdriver.Chrome(service=service, options=chrome_options)
-#driver=webdriver.Chrome(service=Service(executable_path=driver_path),chrome_options=chrome_options)
 
-# Acceder a la página con Selenium
+# Access the page with Selenium
 driver.get(url)
+print("Acceso a la página...")
+driver.find_element(By.XPATH,'//*[@id="main-display"]/div[2]/div[4]/div[1]/label')\
+    .click()
+print("Deseleccionamos TAF")
+driver.find_element(By.XPATH,'//*[@id="go_btn"]')\
+    .click()
+print("Load data")
+time.sleep(5)
 
-# Esperar a que el elemento con id "data-container" esté presente en el DOM
+
+# Wait for the element with id "data-container" to be present in the DOM
 data_container = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, 'data-container'))
 )
 
-# Imprimir el contenido del div
+# Print the content of the div
 print(data_container.text)
 
-# Cerrar el navegador
+# Save the data to the text file
+with open(fileout, 'w', encoding='utf-8') as file:
+    file.write(data_container.text)
+
+print("Los datos se han guardado en el txt", fileout)
+
+# Close the browser
 driver.quit()
